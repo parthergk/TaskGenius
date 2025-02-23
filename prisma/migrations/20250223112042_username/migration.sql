4e6fe50762e289589327d5e952c9d87d4c3e6cce
parthergk <gauravkumar81464@gmail.com>
2025-02-23 16:51:19 +0530

    username insteand of email in db

---
+++ prisma/migrations/20250223112042_username/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Task` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Task` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "Task" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "email",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "username" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
